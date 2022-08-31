d627b8fad6df7cf193f7ea7dea6bab4d66d880a9
dualfade <dualfade@vadersecurity.com>
2022-08-31 11:17:18 -0400

    + spectrwm run file

---
+++ configs/.config/spectrwm/spectrwm_post_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# download and adjust to your needs --
+# @dualfade
+
+/usr/bin/xrandr --output eDP-1 --primary
+/usr/bin/xrandr --output eDP-1 --mode 1920x1080 --auto
+/usr/bin/xrandr --output DP-1 --left-of eDP-1 --auto
+/usr/bin/xrandr --output DP-3 --right-of eDP-1 --mode 3440x1440 --auto
+
+# start other shit --
+# -> yain network-manager-applet
+# required by protonvpn-cli; stupid dep  --
+# /usr/bin/nm-applet &
+#/usr/bin/feh --bg-fill ~/.wallpaper/dark-cubes-hd-wallpaper.jpg &
+/usr/bin/xrdb -merge ~/.Xresources &
+
+# update xps display --
+# xrandr | grep -w connected | cut -f '1' -d ' '
+/usr/bin/xrandr --output eDP-1 --brightness 1.2
