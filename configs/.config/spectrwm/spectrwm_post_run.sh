57c93b6d1d5458b6df3f62cc8d23096f032e5804
dualfade <dualfade@vadersecurity.com>
2023-03-01 17:21:37 -0500

    + update spectrwm_post_run.sh

--- configs/.config/spectrwm/spectrwm_post_run.sh
+++ configs/.config/spectrwm/spectrwm_post_run.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-# download and adjust to your needs --
 # @dualfade
 
+# conected displays --
+# xrandr | grep -w connected | cut -f '1' -d ' '
+
+# laptop; primary --
 /usr/bin/xrandr --output eDP-1 --primary
 /usr/bin/xrandr --output eDP-1 --mode 1920x1080 --auto
-/usr/bin/xrandr --output DP-1 --left-of eDP-1 --auto
+/usr/bin/xrandr --output eDP-1 --brightness 1.2
+
+# decgear overhead --
+# /usr/bin/xrandr --output DP-1 --left-of eDP-1 --auto
+# G: 2.0 R: 47 B: 46 G: 52
+/usr/bin/xrandr --output DP-1 --left-of eDP-1 --mode 2560x1440 --auto
+/usr/bin/xrandr --output DP-1 --brightness 1.0
+
+# Desktop main --
 /usr/bin/xrandr --output DP-3 --right-of eDP-1 --mode 3440x1440 --auto
+/usr/bin/xrandr --output DP-3 --brightness 1.0
 
 # start other shit --
-# -> yain network-manager-applet
-# required by protonvpn-cli; stupid dep  --
-# /usr/bin/nm-applet &
-
-# set wallpaper; anything else ??
-#/usr/bin/feh --bg-fill ~/.wallpaper/dark-cubes-hd-wallpaper.jpg &
-#/usr/bin/xrdb -merge ~/.Xresources &
-
-# update xps display --
-# xrandr | grep -w connected | cut -f '1' -d ' '
-/usr/bin/xrandr --output eDP-1 --brightness 1.2
+/usr/bin/feh --bg-fill ~/.wallpaper/232568_scaled.jpg &
+/usr/bin/xrdb -merge ~/.Xresources &
