7986afd245c8d026e5a084406b524883abdb1e7c
dualfade <dualfade@vadersecurity.com>
2023-10-29 19:07:58 -0400

    + spectrwm 3.5.0 fixins

--- configs/.config/spectrwm/baraction.sh
+++ configs/.config/spectrwm/baraction.sh
@@ -2,14 +2,12 @@
 # Requires: acpi, iostat, lm-sensors, pamixer.
 
 #DISK
-
 hdd() {
     hdd="$(df -h /home | grep /dev | awk '{print $3 "/" $5}')"
     echo -e "$hdd"
 }
 
 #RAM
-
 mem() {
     used="$(free -h | grep Mem: | awk '{print $3}')"
     total="$(free -h| grep Mem: | awk '{print $2}')"
@@ -20,7 +18,6 @@ mem() {
 }
 
 #CPU
-
 cpu() {
     read cpu a b c previdle rest < /proc/stat
     prevtotal=$((a+b+c+previdle))
@@ -32,35 +29,13 @@ cpu() {
 }
 
 #VOLUME
-
 vol() {
     vol="$(pulsemixer --get-volume)"
     echo -e "$vol"
 }
 
-#vol() {
-#	vol="$(amixer -D default get Master | awk -F'[][]' 'END{ print $2 }')"
-#	echo -e j $vol"
-#}
-
-#MIC
-
-# mic() {
-#     state="$()"
-#     if [ state = 1 ]; then
-#         echo "on"
-#     else
-#         echo "off"
-#     fi
-# }
-
 #VPN
-
 vpn() {
-    # Nc='\033[0m'
-    # Red='\033[0;31m'
-    # Green='\033[0;32m'
-    # state="$(ip a | grep 'wg0\|proton0\|tun0' | grep inet | wc -l)"
     state="$(ip a sh | grep -E "wg[0-9].*?|tun[0-9].*?" | grep -c inet)"
    if [ "$state" = 1 ]; then
         echo -e "on"
@@ -70,21 +45,18 @@ vpn() {
 }
 
 #WEATHER
-
 weather() {
     wthr="$(sed 20q ~/.config/weather.txt | grep value | awk '{print $2 $3}' | sed 's/"//g')"
     echo " $wthr"
 }
 
 #TEMP
-
 temp() {
     tmp="$(grep temp_C ~/.config/weather.txt | awk '{print $2}' | sed 's/"//g' | sed 's/,/ C/g')"
     echo " $tmp"
 }
 
 #BATTERY
-
 bat() {
     batstat="$(cat /sys/class/power_supply/BAT0/status)"
     battery="$(cat /sys/class/power_supply/BAT0/capacity)"
@@ -108,7 +80,6 @@ bat() {
 }
 
 #NETWORK
-
 network() {
     # wire="$(ip a | grep -E 'enp.*?' | grep 'inet' | wc -l)"
     # wifi="$(ip a | grep -E 'wlan[0-9].*?' | grep 'inet' | wc -l)"
@@ -125,17 +96,14 @@ network() {
 }
 
 # UTC CLOCK
-
 universal_time() {
   TZ=UTC date -R | awk '{print $4,$5,$6}'
 }
 
 # FINAL BAR
-
 SLEEP_SEC=2
 #loops forever outputting a line every SLEEP_SEC secs
     while :; do
-      # echo "$(cpu) |  $(mem) |  $(hdd) |  $(vpn) |  $(vol) | $(bat) | $(network) | $(universal_time) |"
-      echo "+@fg=1; $(cpu) |  $(mem) |  $(hdd) |  $(vpn) |  $(vol) | $(bat) | $(network) +@fg=2; | $(universal_time) +@fg=0;|"
+      echo "+@fg=1; $(cpu) |  $(mem) |  $(hdd) |  $(vpn) |  $(vol) | $(bat) | $(network) +@fg=2; | $(universal_time) +@fg=0;"
         sleep $SLEEP_SEC
     done
